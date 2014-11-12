3384f4021e665eff48c0ed2eb91de196948ce19c
Paul Wolneykien <manowar@altlinux.org>
2014-11-12 22:48:53 +0300

    Add wrapper for '*-[0-9].jpg' et al. files

---
+++ jpegsum.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -efu
+
+
+PROG_NAME='jpegsum'
+STATNAME="jpegsum.txt"
+CLASSPATH='class'
+MAINCLASS='name.wolneykien.filenamestats.FileNameStats'
+PATTERN='-([0-9]+).[Jj][Pp][Ee]?[Gg]$'
+
+java -cp "$CLASSPATH" \
+     -Dprogram.name="$PROG_NAME" \
+     "$MAINCLASS" "$PATTERN" "$STATNAME" "$@"
